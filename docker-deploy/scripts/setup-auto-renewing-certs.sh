ea7e1145b905617eaa0640bd2d63d534b332df71
Сергей Тяпкин <Tyapkin2002@mail.ru>
2024-09-30 01:48:39 +0300

    fix renew-certs

--- docker-deploy/scripts/setup-auto-renewing-certs.sh
+++ docker-deploy/scripts/setup-auto-renewing-certs.sh
@@ -3,7 +3,7 @@ sudo apt-get install cron
 echo "----------------------------------------------------------------------------------------------"
 echo "Now you must add this string-command in your crontab to auto executing it each 1 month. Command:"
 echo ""
-echo "0 0 1 * * cd $(pwd) && make run renew-certs >> ./certbot-renew.log"
+echo "0 0 1 * * cd $(pwd) && make renew-certs >> ./certbot-renew.log"
 echo ""
 echo "Copy that command and [press Enter]. Then add this string in end of opened file."
 read ENTER
