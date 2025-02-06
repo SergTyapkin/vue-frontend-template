d3779a5f70b23a1617d9e1e9afc730abb62a3555
Сергей Тяпкин <Tyapkin2002@mail.ru>
2025-02-06 13:02:15 +0300

    small minor text fixes

--- docker-deploy/scripts/show-variables-to-github-ci.sh
+++ docker-deploy/scripts/show-variables-to-github-ci.sh
@@ -1,6 +1,7 @@
 echo "Now read README.md and setup another deploy environments variables."
 echo "It seems like values must be:"
 echo ""
+echo "SSH_DEPLOY_KEY=<private-key-that-you've-seen-before>  [! MUST BE SECRET VARIABLE !]"
 echo "SERVER_USERNAME=$(whoami)  [! MUST BE SECRET VARIABLE !]"
 echo "DEPLOY_HOST=$(. "docker-deploy/.env"; echo "$VITE_DEPLOY_HOSTNAME")"
 echo "PROJECT_PATH=$(pwd)"
