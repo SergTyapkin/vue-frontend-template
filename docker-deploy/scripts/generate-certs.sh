8bf2c986f6851c771550e1f807ae5baca9bc61f0
Сергей Тяпкин <Tyapkin2002@mail.ru>
2025-02-21 05:48:50 +0300

    fix script

--- docker-deploy/scripts/generate-certs.sh
+++ docker-deploy/scripts/generate-certs.sh
@@ -1,5 +1,5 @@
 cd docker-deploy || exit
 docker compose --env-file ../.env down
 docker compose --env-file ../.env up -d nginx-certbot
-docker compose --env-file ../.env run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d "$(. "docker-deploy/.env"; echo "$VITE_DEPLOY_HOSTNAME")"
+docker compose --env-file ../.env run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d "$(. "./.env"; echo "$VITE_DEPLOY_HOSTNAME")"
 sudo chmod ugo+rwx -R ./certbot/
