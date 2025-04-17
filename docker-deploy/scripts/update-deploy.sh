bd7cb81d506804a14e0fe0e2fec5e3f6c1c197f6
Сергей Тяпкин <Tyapkin2002@mail.ru>
2025-04-17 03:28:34 +0300

    fix update script

--- docker-deploy/scripts/update-deploy.sh
+++ docker-deploy/scripts/update-deploy.sh
@@ -2,6 +2,6 @@ git fetch --all
 git reset --hard "origin/$(. ".env"; echo "$DEPLOY_BRANCH")"
 echo "Deploying last commit:"
 git log --oneline -1
-make build
-make run
+make build &&
+make run &&
 echo "Frontend updated successfully"
