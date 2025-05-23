e4bd9ae226747ec457f3581d80fca3ceb27be3f1
Сергей Тяпкин <Tyapkin2002@mail.ru>
2025-05-24 00:37:05 +0300

    fix build

--- docker-deploy/scripts/update-deploy.sh
+++ docker-deploy/scripts/update-deploy.sh
@@ -1,5 +1,5 @@
 git fetch --all &&
-git reset --hard "origin/$(. ".env"; echo "$DEPLOY_BRANCH")" &&
+git reset --hard "origin/$(. "./.env"; echo "$DEPLOY_BRANCH")" &&
 echo "Deploying last commit:" &&
 git log --oneline -1 &&
 bash ./docker-deploy/scripts/build.sh &&
