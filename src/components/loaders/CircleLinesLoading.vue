924a89f7a826c5fe019786ba152ec9d743ea1478
Сергей Тяпкин <Tyapkin2002@mail.ru>
2025-05-26 23:31:21 +0300

    Add loaders

---
+++ src/components/loaders/CircleLinesLoading.vue
@@ -0,0 +1,54 @@
+<style lang="stylus" scoped>
+@import '../../styles/constants.styl'
+@import '../../styles/components.styl'
+@import '../../styles/buttons.styl'
+@import '../../styles/fonts.styl'
+@import '../../styles/utils.styl'
+@import '../../styles/animations.styl'
+@import '../../styles/scrollbars.styl'
+
+borderWidth = 3px
+
+.circle-loading
+  width 'calc(var(--size) - %s)' % borderWidth
+  height 'calc(var(--size) - %s)' % borderWidth
+  border borderWidth solid colorBlockBg
+  border-radius 50%
+  position relative
+  box-sizing border-box
+  animation rotation 1s linear infinite
+  &::after
+    content ''
+    box-sizing border-box
+    position absolute
+    left 50%
+    top 50%
+    transform translate(-50%, -50%)
+    width var(--size)
+    height var(--size)
+    border-radius 50%
+    border borderWidth solid
+    border-color colorEmp1 transparent
+
+  @keyframes rotation
+    0%
+      transform rotate(0)
+    100%
+      transform rotate(360deg)
+</style>
+
+<template>
+  <div class="circle-loading" :style="{ '--size': size }" :class="{centered}" />
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    size: {
+      type: String,
+      default: '40px',
+    },
+    centered: Boolean,
+  },
+};
+</script>
---
+++ src/components/loaders/PointsLoading.vue
@@ -0,0 +1,56 @@
+<style lang="stylus" scoped>
+@import '../../styles/constants.styl'
+@import '../../styles/components.styl'
+@import '../../styles/buttons.styl'
+@import '../../styles/fonts.styl'
+@import '../../styles/utils.styl'
+@import '../../styles/animations.styl'
+@import '../../styles/scrollbars.styl'
+
+.circle-loading
+  width calc(var(--size) / 4)
+  height calc(var(--size) / 4)
+  margin-top calc(var(--size) / 4 * 1.5)
+  margin-bottom calc(var(--size) / 4 * 1.5)
+  position relative
+  border-radius 50%
+  color colorEmp1
+  animation fill 1s ease-in infinite alternate
+  &::before
+  &::after
+    content ''
+    position absolute
+    height 100%
+    width 100%
+    border-radius 50%
+    left calc(var(--size) / 4 * 1.5)
+    top 0
+    animation fill 0.9s ease-in infinite alternate
+
+  &::after
+    left auto
+    right calc(var(--size) / 4 * 1.5)
+    animation-duration 1.1s
+
+  @keyframes fill
+    0%
+      box-shadow 0 0 0 -1px inset
+    100%
+      box-shadow 0 0 0 calc(var(--size) / 8) inset
+</style>
+
+<template>
+  <div class="circle-loading" :style="{ '--size': size }" :class="{centered}" />
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    size: {
+      type: String,
+      default: '60px',
+    },
+    centered: Boolean,
+  },
+};
+</script>
